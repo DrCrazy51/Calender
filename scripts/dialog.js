676c27069b363537ba6776114720436228fbafdb
DrCrazy51 <135680448+DrCrazy51@users.noreply.github.com>
2025-06-09 23:09:26 -0500

    Create button close and dialog css

---
+++ scripts/dialog.js
@@ -0,0 +1,13 @@
+export function initDialog(name) {
+  const dialogElement = document.querySelector(`[data-dialog=${name}]`);
+
+  return {
+    open() {
+      dialogElement.showModal();
+    },
+    close() {
+      dialogElement.close();
+    }
+  }
+
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,10 @@
 
 import { initViewSelect } from "./view-select.js";
 import { initCalendar } from "./calendar.js";
+import { initEventCreateButton } from "./event-create-button.js";
+import { initEventFormDialog } from "./event-form-dialog.js";
+
 initViewSelect();
+initEventCreateButton();
+initEventFormDialog();
 initCalendar();
