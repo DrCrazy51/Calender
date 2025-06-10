e1fe9780741742fc2ee6fcbfee0ad70ffab0d4df
DrCrazy51 <135680448+DrCrazy51@users.noreply.github.com>
2025-06-10 02:10:22 -0500

    Create Event update

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -1,5 +1,12 @@
 export function initDialog(name) {
   const dialogElement = document.querySelector(`[data-dialog=${name}]`);
+  const closeButtons = document.querySelectorAll("[data-dialog-close-button]");
+
+  for (const closeButton of closeButtons) {
+    closeButton.addEventListener("click", () => {
+      dialogElement.close();
+    });
+  }
 
   return {
     open() {
