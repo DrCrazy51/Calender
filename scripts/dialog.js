228ea450715b893704460c12aa5b52ca02397560
DrCrazy51 <135680448+DrCrazy51@users.noreply.github.com>
2025-06-10 03:24:30 -0500

    create event exit and cancel

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -1,20 +1,47 @@
+import { waitUntilAnimationsFinish } from "./animation.js";
+
 export function initDialog(name) {
   const dialogElement = document.querySelector(`[data-dialog=${name}]`);
   const closeButtons = document.querySelectorAll("[data-dialog-close-button]");
 
+
+  function close() {
+    dialogElement.classList.add("dialog--closing");
+
+    waitUntilAnimationsFinish(dialogElement)
+      .then(() => {
+        dialogElement.classList.remove("dialog--closing");
+        dialogElement.close();
+      })
+      .catch((error) => {
+        console.error("Finish dialog animation promise failed", error);
+      });
+  }
+
   for (const closeButton of closeButtons) {
     closeButton.addEventListener("click", () => {
-      dialogElement.close();
+      close();
     });
   }
+
+  dialogElement.addEventListener("click", (event) => {
+    if (event.target === dialogElement) {
+      close();
+    }
+  });
+
+  dialogElement.addEventListener("cancel", (event) => {
+    event.preventDefault();
+    close();
+  });
 
   return {
     open() {
       dialogElement.showModal();
     },
     close() {
-      dialogElement.close();
+      close();
     }
-  }
+  };
 
 }
